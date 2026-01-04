bfdf7864673a79022fbe68f227dbff38e7e214d6
Viktoriia <vikatori130201@gmail.com>
2026-01-04 23:32:31 +0300

    myHW

---
+++ src/task-2.ts
@@ -0,0 +1,14 @@
+const product: Product = {
+    id: 1,
+    title: "Tablet",
+    description: "Compact and fast",
+  };
+  
+  console.log(`Product: ${JSON.stringify(product)}`);
+
+  
+interface Product {
+    readonly id: number;
+    title: string;
+    description?: string;
+  }
